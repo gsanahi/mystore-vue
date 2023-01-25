58ec724a68ba4b0cec8f115ae2ab5e5e8d80f9c8
Anahi Garcia <gsanahi@gmail.com>
2023-01-25 14:36:47 +0100

    model User creation

---
+++ src/models/users.ts
@@ -0,0 +1,12 @@
+// modelo de Usuario
+
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  role: string;
+  avatar: string;
+  creationAt: string;
+  updatedAt: string;
+}
