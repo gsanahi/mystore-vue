f88b0a62dc13b13cd49d69935b07852c7db8984c
Anahi Garcia <gsanahi@gmail.com>
2023-01-28 12:03:39 +0100

    Product view with fetch

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,39 @@
+<template>
+  <n-card :title="product.title" hoverable>
+    <template #cover>
+      <img :src="product.images[0]" />
+    </template>
+    {{ product.description }}
+    <n-space>
+      <n-button size="large">
+        <template #icon>
+          <n-icon>
+            <MoneyBillWave />
+          </n-icon>
+        </template>
+        {{ product.price }}
+      </n-button>
+    </n-space>
+  </n-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { NButton, NCard, NSpace, NIcon } from "naive-ui";
+import { MoneyBillWave as MoneyBillWave } from "@vicons/fa";
+import type { Product } from "@/models/product";
+
+export default defineComponent({
+  name: "ProductDetails",
+  props: {
+    product: { type: Object as PropType<Product>, required: true },
+  },
+  components: {
+    NButton,
+    NCard,
+    NSpace,
+    NIcon,
+    MoneyBillWave,
+  },
+});
+</script>
