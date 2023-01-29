c8a83faca48369c468242838b613a988a2a60eb9
Anahi Garcia <gsanahi@gmail.com>
2023-01-29 13:41:51 +0100

    validacion login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,42 +1,58 @@
 <template>
   <n-card title="Login">
-    <div class="card-content">
-      <div class="form-group">
-        <n-auto-complete
-          v-model:value="email"
-          :options="options"
-          placeholder="Email"
-          required="true"
-        />
+    <spinner-load :show="loading">
+      <div class="card-content">
+        <div class="form-group">
+          <n-auto-complete
+            v-model:value="email"
+            :options="options"
+            placeholder="Email"
+            required="true"
+          />
+        </div>
+        <div class="form-group">
+          <n-input
+            v-model:value="password"
+            type="password"
+            show-password-on="mousedown"
+            placeholder="Password"
+            @keyup.enter="handleLogin"
+            :maxlength="15"
+          />
+        </div>
+        <n-button
+          strong
+          secondary
+          block
+          round
+          type="primary"
+          @click="handleLogin"
+          >Login</n-button
+        >
+        <n-alert v-if="error" title="Error" type="error">
+          {{ error }}
+        </n-alert>
       </div>
-      <div class="form-group">
-        <n-input
-          v-model:value="password"
-          type="password"
-          show-password-on="mousedown"
-          placeholder="Password"
-          :maxlength="15"
-        />
-      </div>
-      <n-button
-        strong
-        secondary
-        block
-        round
-        type="primary"
-        @click="handleLogin(email, password)"
-        >Login</n-button
-      >
-    </div>
+    </spinner-load>
   </n-card>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed } from "vue";
-import { NAutoComplete, NButton, NCard, NInput } from "naive-ui";
+import { NAlert, NAutoComplete, NButton, NCard, NInput } from "naive-ui";
+import SpinnerLoad from "./SpinnerLoad.vue";
+
+// Código de: https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+const validateEmail = (email: string) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 
 export default defineComponent({
-  components: { NAutoComplete, NButton, NCard, NInput },
+  components: { NAlert, NAutoComplete, NButton, NCard, NInput, SpinnerLoad },
   setup() {
     const emailRef = ref("");
     const passRef = ref("");
@@ -44,6 +60,8 @@ export default defineComponent({
     return {
       email: emailRef,
       password: passRef,
+      loading: ref(false),
+      error: ref(""),
       options: computed(() => {
         return ["@gmail.com", "@mail.com", "@qq.com"].map((suffix) => {
           const prefix = emailRef.value.split("@")[0];
@@ -56,10 +74,28 @@ export default defineComponent({
     };
   },
   methods: {
-    handleLogin(email: string, password: string) {
+    handleLogin() {
+      this.error = "";
+      if (!this.email || !validateEmail(this.email)) {
+        this.error = "Email Inválido";
+        return;
+      }
+      if (!this.password) {
+        this.error = "Contraseña Inválida";
+        return;
+      }
+      this.loading = true;
       this.$store
-        .dispatch("user/login", { email, password })
-        .then(() => this.$router.push({ name: "home" }));
+        .dispatch("user/login", { email: this.email, password: this.password })
+        .then(() => this.$router.push({ name: "home" }))
+        .catch((error) => {
+          if (error.response?.status === 401) {
+            this.error = "Email o contraseña inválida";
+          } else {
+            this.error = error.message;
+          }
+        })
+        .finally(() => (this.loading = false));
     },
   },
 });
@@ -80,6 +116,10 @@ export default defineComponent({
 .form-group,
 .n-button {
   margin: 1rem;
+  width: 60%;
+}
+
+.n-alert {
   width: 60%;
 }
 </style>
